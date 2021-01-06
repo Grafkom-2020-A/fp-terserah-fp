2c1596c5318e59cf05859ea331621dfd3e44feda
aulkiller <58225261+aulkiller@users.noreply.github.com>
2021-01-06 20:34:33 +0700

    geser kamera+light+gedung+awan

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function setup() {
         //SEMUA YANG ADA SANGKUT PAUT SAMA THREE.JS DISINI
 
 
-        var camera = initCamera(new THREE.Vector3(20, 0, 150));
+        var camera = initCamera(new THREE.Vector3(0, 0, 150));
         var scene = new THREE.Scene();
         var webGLRenderer = initRenderer();
         var trackballControls = initTrackballControls(camera, webGLRenderer);
@@ -97,8 +97,46 @@ function setup() {
             sprite.position.set(Math.random() * range - range / 2, Math.random() * range - range / 2, Math.random() *
               range - range / 2);
             sprite.scale.set(5, 5, 5);
-            scene.add(sprite);
+            // scene.add(sprite);
           }
+
+          hlight = new THREE.AmbientLight (0x404040,100);
+          scene.add(hlight);
+          directionalLight = new THREE.DirectionalLight(0xffffff,100);
+          directionalLight.position.set(0,1,0);
+          directionalLight.castShadow = true;
+          scene.add(directionalLight);
+          light = new THREE.PointLight(0xc4c4c4,10);
+          light.position.set(0,300,500);
+          scene.add(light);
+          light2 = new THREE.PointLight(0xc4c4c4,10);
+          light2.position.set(500,100,0);
+          scene.add(light2);
+          light3 = new THREE.PointLight(0xc4c4c4,10);
+          light3.position.set(0,100,-500);
+          scene.add(light3);
+          light4 = new THREE.PointLight(0xc4c4c4,10);
+          light4.position.set(-500,300,500);
+          scene.add(light4);
+
+          var loader = new THREE.GLTFLoader();
+          loader.load('../gLTF/scene.gltf', function (result) {
+            // correctly position the scene
+            result.scene.position.set(0,-50,0)
+            result.scene.scale.set(0.05, 0.05, 0.05);
+            scene.add(result.scene)
+          });
+
+          // initDefaultLighting(scene);
+          var loader = new THREE.GLTFLoader();
+          loader.load('../gLTF/clouds.gltf', function (result) {
+            // correctly position the scene
+            result.scene.position.set(0,20,0)
+            // result.scene.scale.set(10, 10, 10);
+            result.scene.scale.set(3, 3, 3);
+            scene.add(result.scene)
+          });
+          
         }
       
         var step = 0;
